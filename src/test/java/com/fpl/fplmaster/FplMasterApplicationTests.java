ce00f65cc9a919f0aec5195e82f757e7b63fe317
sriranjith <sriranjith23@gmail.com>
2021-07-17 20:29:04 +0530

    Static

--- src/test/java/com/fpl/fplmaster/FplMasterApplicationTests.java
+++ src/test/java/com/fpl/fplmaster/FplMasterApplicationTests.java
@@ -1,9 +1,7 @@
 package com.fpl.fplmaster;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FplMasterApplicationTests {
 
     @Test
